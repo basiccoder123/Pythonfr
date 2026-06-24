467814f9a636b85224a95a6e35b216ab2b60a6df
basiccoder123 <kirkffrimpong@gmail.com>
2026-06-24 14:42:04 +0000

    condstate

---
+++ condstate.py
@@ -0,0 +1,17 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("All the numbers have boolean value as true.")
+else:
+    print("Atleast one number has boolean value at false.")
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("Either of the numbers are greater than zero.")
+else:
+    print("None of the numbers are greaer than zero.")
