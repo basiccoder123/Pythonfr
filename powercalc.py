f60e3423844bb69ee8b91b7527f8b1fc93fca311
basiccoder123 <kirkffrimpong@gmail.com>
2026-07-07 01:44:28 +0000

    projects

--- powercalc.py
+++ powercalc.py
@@ -1,2 +1,6 @@
+base_num = int(input("Enter a base number: "))
+exp_num = int(input("Enter an exponent: "))
 
+result = base_num ** exp_num
+print(f"{base_num} raised to the power of {exp_num} is: {result}")
 
