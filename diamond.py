d7d4d02386c43b9317449ca64f5f6e27cd2f53b3
basiccoder123 <kirkffrimpong@gmail.com>
2026-07-09 15:00:59 +0000

    pyramids

---
+++ diamond.py
@@ -0,0 +1,33 @@
+rowSize = int(input("enter the number of rows: "))
+if rowSize%2==0: #conditions
+  halfDiamRow = int(rowSize/2)
+else:
+  halfDiamRow = int(rowSize/2)+1
+
+# blank spaces printed before each of the numbers in every row
+space = halfDiamRow-1
+#loop for upper part
+# value of i in the outer loop
+# each value of i represents a row
+for i in range(1, halfDiamRow+1): #loop for rows
+  for j in range(1, space+1): #loop for columns
+    print(end=" ")
+  space = space-1 # each new row shifts; before printing the numbers the spaces are going to be...
+  num = 1 # restarting the number
+  for j in range(2*i-1):
+    print(end=str(num))
+  #incerementing number at each column
+    num = num+1
+  print()
+space = 1
+#loop for lower part
+for i in range(1, halfDiamRow): #loop for rows
+  for j in range(1, space+1):  #loop for columns
+    print(end=" ")
+  space = space+1
+  num = 1
+  for j in range(1, 2*(halfDiamRow-i)):
+    print(end=str(num)) #display result
+  #incerementing number at each column
+    num = num+1
+  print()
---
+++ pyramid.py
@@ -0,0 +1,8 @@
+print("Half a pyramid of ($)")
+n = int(input("Enter the number of rows:"))
+
+for i in range(n):
+
+    for j in range(i + 1):
+        print("$", end="")
+    print()
